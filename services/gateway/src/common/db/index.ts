d50172206649dbe11aab02c09f02b0dafc538c7a
tejaskale99 <tejaskale1556@gmail.com>
2026-09-11 12:55:08 +0530

    fix(db): update database connection to use config object instead of environment variable

--- services/gateway/src/common/db/index.ts
+++ services/gateway/src/common/db/index.ts
@@ -1,4 +1,5 @@
-import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import config from "../config/index.js";
+import * as schema from "./schema.js";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+export const db = drizzle(config.database.url, { schema });
