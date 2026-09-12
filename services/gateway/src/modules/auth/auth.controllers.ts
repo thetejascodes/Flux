43aa0f1df1d74b201f03af425aaefc19908f5216
tejaskale99 <tejaskale1556@gmail.com>
2026-09-12 13:13:15 +0530

    feat(auth): addverifyOtp controller

--- services/gateway/src/modules/auth/auth.controllers.ts
+++ services/gateway/src/modules/auth/auth.controllers.ts
@@ -18,3 +18,28 @@ const requestOtp = async (
   }
 };
 
+const verifyOtp = async (req: Request<{}, {}, VerifyOtpInput>,
+  res: Response,
+  next: NextFunction,) => {
+  try {
+    const {phone,code} = req.body; 
+    const result = await otpService.verifyOtp({phone,code});
+    return ApiResponse.ok(res,"Otp Verified",result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const signUp = async (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
+
+const login = async (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
+const refresh = async (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
