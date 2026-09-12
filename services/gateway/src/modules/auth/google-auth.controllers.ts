b10a2ab114c3f861243ac6d77f6c262657a39092
tejaskale99 <tejaskale1556@gmail.com>
2026-09-12 20:02:00 +0530

    feat(auth): implement googleCallback function for handling Google login response

--- services/gateway/src/modules/auth/google-auth.controllers.ts
+++ services/gateway/src/modules/auth/google-auth.controllers.ts
@@ -1,7 +1,25 @@
 import * as googleAuthService from "./google-auth.service.js";
 import ApiResponse from "../../common/utils/api-response.js";
 import type { Request, Response, NextFunction } from "express";
+import ApiError from "../../common/utils/api-error.js";
 
-const googleRedirect = async(req:Request,res:Response)=>{
-    return res.redirect(googleAuthService.getGoogleAuthUrl());
+const googleRedirect = async (req: Request, res: Response) => {
+  return res.redirect(googleAuthService.getGoogleAuthUrl());
+};
+
+const googleCallback = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const code = req.query.code as string | undefined;
+    if (!code) {
+      throw ApiError.badRequest("Missing authorization code from Google");
+    }
+    const result = await googleAuthService.loginWithGoogle(code);
+    return ApiResponse.ok(res, "Google login successful", result);
+  } catch (error) {
+    next(error);
+  }
 };
