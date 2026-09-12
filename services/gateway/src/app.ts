be8e8571c122ea7065e9ce5ae55306f23d963f74
tejaskale99 <tejaskale1556@gmail.com>
2026-09-13 03:01:10 +0530

    feat(app): reorganize middleware imports and add catalog proxy route

--- services/gateway/src/app.ts
+++ services/gateway/src/app.ts
@@ -1,14 +1,19 @@
 import express from "express";
 import errorHandler from "./common/middleware/errorHandler.js";
 import authRoutes from "./modules/auth/auth.routes.js";
-const app = express();
+import proxyTo from "./common/utils/proxy.utils.js";
+import isAuthenticated from "./modules/auth/auth.middleware.js";
+import config from "./common/config/index.js";
 
-app.use(express.json());
+const app = express();
 
 app.get("/health", (req, res) => {
   res.json({ status: "ok" });
 });
 
+app.use("/catalog", isAuthenticated, proxyTo(config.services.catalogUrl));
+
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 app.use(errorHandler);
