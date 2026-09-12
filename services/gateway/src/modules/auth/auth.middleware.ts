5f0c9addfa60043025609ac0616513d7c8c72af1
tejaskale99 <tejaskale1556@gmail.com>
2026-09-12 23:25:10 +0530

    feat(auth): implement authentication middleware and extend Express Request type with userId

---
+++ services/gateway/src/modules/auth/auth.middleware.ts
@@ -0,0 +1,22 @@
+import { verifyAccessToken } from "../../common/utils/jwt.utils.js";
+import type { Request, Response, NextFunction } from "express";
+import ApiError from "../../common/utils/api-error.js";
+
+const isAuthenticated = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const header = req.headers.authorization;
+    if (!header?.startsWith("Bearer ")) {
+      throw ApiError.unauthorized("No token provided");
+    }
+    const token = header.replace("Bearer ", "");
+    const payload = verifyAccessToken(token);
+    req.userId = payload.userId;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
