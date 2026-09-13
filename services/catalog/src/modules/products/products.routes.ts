3295501375beccdb066835c3dfa7dc9d1df49db8
tejaskale99 <tejaskale1556@gmail.com>
2026-09-13 23:36:38 +0530

    feat(routes): add get product by id end to end and add routes

--- services/catalog/src/modules/products/products.routes.ts
+++ services/catalog/src/modules/products/products.routes.ts
@@ -1,10 +1,21 @@
 import { Router } from "express";
 import * as productController from "./products.controllers.js";
 import validate, { validateQuery } from "../../common/middlewares/validate.js";
-import { CreateProductDto,ListProductsQueryDto,UpdateProductDto } from "./dto/products.dto.js";
+import {
+  CreateProductDto,
+  ListProductsQueryDto,
+  UpdateProductDto,
+} from "./dto/products.dto.js";
 
 const router = Router();
 
-router.get("/",validateQuery(ListProductsQueryDto),productController.listProducts)
+router.get(
+  "/",
+  validateQuery(ListProductsQueryDto),
+  productController.listProducts,
+);
+
+router.get("/:id", productController.getProductById);
 
 export default router;
+
