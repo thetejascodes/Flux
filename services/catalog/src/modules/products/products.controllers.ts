873e1ebdbfa5d61a7b10519897ec6259649536d6
tejaskale99 <tejaskale1556@gmail.com>
2026-09-13 20:14:16 +0530

    feat(controller): add listProducts controller for fetch products by query

--- services/catalog/src/modules/products/products.controllers.ts
+++ services/catalog/src/modules/products/products.controllers.ts
@@ -12,19 +12,36 @@ const createProduct = async (
   res: Response,
   next: NextFunction,
 ) => {
-    try {
-        const result = await productService.createProduct(req.body);
-        return ApiResponse.created(res,"Product created successfully",result);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const result = await productService.createProduct(req.body);
+    return ApiResponse.created(res, "Product created successfully", result);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const getProductById = async(req:Request,res:Response,next:NextFunction)=>{
-    try {
-        const result = await productService.getProductById(req.params.id as string);
-        return ApiResponse.ok(res,"Product fetched successfully",result);
-    } catch (error) {
-        next(error);
-    }
-}
+const getProductById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const result = await productService.getProductById(req.params.id as string);
+    return ApiResponse.ok(res, "Product fetched successfully", result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const listProducts = async (
+  req: Request<{}, {}, {}, ListProductsQueryInput>,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const result = await productService.listProducts(req.query);
+    return ApiResponse.ok(res, "Products fetched successfully", result);
+  } catch (error) {
+    next(error);
+  }
+};
