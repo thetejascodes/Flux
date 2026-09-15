70486ae3ee01b47609da533b0b1b301e94122195
tejaskale99 <tejaskale1556@gmail.com>
2026-09-15 15:23:30 +0530

    fix(app): reorder  and stockRoutes monted middleware and import statements for clarity

--- services/inventory/src/app.ts
+++ services/inventory/src/app.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import errorHandler from "./common/middleware/errorHandler.js";
+import stockRoutes from "./modules/stock/stock.routes.js";
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/health", (req, res) => {
   res.json({ status: "ok" });
 });
 
-app.use(express.json());
-
+app.use("/reservations", stockRoutes);
 app.use(errorHandler);
+
 export default app;
