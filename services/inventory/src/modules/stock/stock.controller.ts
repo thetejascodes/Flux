5b500ef40d748bd63c76ce34dc227a1a084bd0c0
tejaskale99 <tejaskale1556@gmail.com>
2026-09-15 15:04:18 +0530

    feat(stock): implement reserveStock controller for stock reservation

---
+++ services/inventory/src/modules/stock/stock.controller.ts
@@ -0,0 +1,20 @@
+import * as stockService from "./stock.service.js";
+import ApiResponse from "../../common/utils/api-response.js";
+import type { Request, Response, NextFunction } from "express";
+import type {
+  ReserveStockInput,
+  ReservationIdParams,
+} from "./dto/stock.dto.js";
+
+const reserveStock = async (
+  req: Request<{}, {}, ReserveStockInput>,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const result = await stockService.reserveStock(req.body);
+    return ApiResponse.created(res, "Stock reserved successfully", result);
+  } catch (error) {
+    next(error);
+  }
+};
