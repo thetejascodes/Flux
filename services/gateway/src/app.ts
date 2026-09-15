6ca5c8ef572e346c471eaec8d13ff86d9bfd9461
tejaskale99 <tejaskale1556@gmail.com>
2026-09-15 15:41:51 +0530

    fix(app): enable inventory route in the gateway application

--- services/gateway/src/app.ts
+++ services/gateway/src/app.ts
@@ -12,7 +12,7 @@ app.get("/health", (req, res) => {
 });
 
 app.use("/catalog", isAuthenticated, proxyTo(config.services.catalogUrl));
-// app.use("/inventory", isAuthenticated, proxyTo(config.services.inventoryUrl));
+app.use("/inventory", isAuthenticated, proxyTo(config.services.inventoryUrl));
 // app.use("/orders",    isAuthenticated, proxyTo(config.services.orderUrl));
 // app.use("/payments",  isAuthenticated, proxyTo(config.services.paymentUrl));
 // app.use("/delivery",  isAuthenticated, proxyTo(config.services.deliveryUrl));
