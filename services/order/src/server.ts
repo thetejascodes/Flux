fb70ac9cadff965bfae64bc2fffed727b986abf1
tejaskale99 <tejaskale1556@gmail.com>
2026-09-16 15:24:32 +0530

    feat(server): add RabbitMQ connection before starting the server

--- services/order/src/server.ts
+++ services/order/src/server.ts
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import app from "./app.js";
 import config from "./common/config/index.js";
+import { connectRabbitMQ } from "./common/events/connection.js";
 
 const port = config.port;
 
 const startServer = async () => {
+  await connectRabbitMQ();
   app.listen(port, () => {
     console.log(`🚀 Order service running on port ${port}`);
   });
