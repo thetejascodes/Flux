2d249ea6dfc6a9b6ac1db46f0f297f04037d52ff
tejaskale99 <tejaskale1556@gmail.com>
2026-09-17 00:45:19 +0530

    feat(rabbitmq): implement RabbitMQ connection and channel management

--- services/payment/src/server.ts
+++ services/payment/src/server.ts
@@ -1,12 +1,13 @@
 import "dotenv/config";
 import app from "./app.js";
 import config from "./common/config/index.js";
+import { connectRabbitMQ } from "./common/events/connection.js";
 const port = config.port;
 
 const startServer = async () => {
-  
+  await connectRabbitMQ();
   app.listen(port, () => {
-    console.log(`🚀 Order service running on port ${port}`);
+    console.log(`🚀 Payment service running on port ${port}`);
   });
 };
 
