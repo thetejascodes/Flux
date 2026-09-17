4f5685fb53719750d2b05ae2f68d225b9ea28bd5
tejaskale99 <tejaskale1556@gmail.com>
2026-09-17 13:15:53 +0530

    feat(payment): register payment saga handlers during server startup

--- services/payment/src/server.ts
+++ services/payment/src/server.ts
@@ -2,10 +2,12 @@ import "dotenv/config";
 import app from "./app.js";
 import config from "./common/config/index.js";
 import { connectRabbitMQ } from "./common/events/connection.js";
+import { registerPaymentSagaHandlers } from "./modules/payments/payments.gateway.js";
 const port = config.port;
 
 const startServer = async () => {
   await connectRabbitMQ();
+  await registerPaymentSagaHandlers();
   app.listen(port, () => {
     console.log(`🚀 Payment service running on port ${port}`);
   });
