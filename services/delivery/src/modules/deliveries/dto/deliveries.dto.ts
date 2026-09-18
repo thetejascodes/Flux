ef6db8dd016de5d8b6041f68dd31dd48e83525e9
tejaskale99 <tejaskale1556@gmail.com>
2026-09-18 23:40:48 +0530

    feat(deliveries): rename OrderConfirmedDto to AssignDeliveryDto and update related types

--- services/delivery/src/modules/deliveries/dto/deliveries.dto.ts
+++ services/delivery/src/modules/deliveries/dto/deliveries.dto.ts
@@ -1,13 +1,13 @@
 import BaseDto from "../../../common/dto/baseDto.js";
 import { z } from "zod";
 
-class OrderConfirmedDto extends BaseDto {
+class AssignDeliveryDto extends BaseDto {
   static schema = z.object({
     orderId: z.string().uuid(),
     warehouseId: z.string().uuid(),
   });
 }
 
-export type OrderConfirmedInput = z.infer<typeof OrderConfirmedDto.schema>;
+export type AssignDeliveryInput  = z.infer<typeof AssignDeliveryDto.schema>;
 
-export { OrderConfirmedDto };
+export { AssignDeliveryDto };
