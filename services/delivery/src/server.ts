7c84afee17396ef8bc870615972fedf22c3946ed
tejaskale99 <tejaskale1556@gmail.com>
2026-09-18 14:32:48 +0530

    fix(server): use configuration module to set server port

--- services/delivery/src/server.ts
+++ services/delivery/src/server.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import app from "./app.js";
-
-const port = process.env.PORT;
+import config from "./common/config/index.js";
+const port = config.port;
 
 const startServer = async () => {
   app.listen(port, () => {
