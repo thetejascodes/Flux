4c5d9f9f2bc7d8bb37193566bb0d55664f753d74
tejaskale99 <tejaskale1556@gmail.com>
2026-09-18 15:16:29 +0530

    feat(tracing): add dotenv import for environment variable configuration

--- services/inventory/src/common/tracing.ts
+++ services/inventory/src/common/tracing.ts
@@ -1,3 +1,4 @@
+import "dotenv/config"
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
