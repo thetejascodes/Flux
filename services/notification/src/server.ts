5103365ce6523b833ff89f48be6b5c075114282c
tejaskale99 <tejaskale1556@gmail.com>
2026-09-20 01:14:33 +0530

    feat(server): establish RabbitMQ connection on server start

--- services/notification/src/server.ts
+++ services/notification/src/server.ts
@@ -1,9 +1,11 @@
 import "dotenv/config";
 import app from "./app.js";
 import config from "./common/config/index.js";
+import { connectRabbitMQ } from "./common/events/connection.js";
 const port = config.port;
 
 const startServer = async () => {
+  await connectRabbitMQ();
   app.listen(port, () => {
     console.log(`🚀 Notification service running on port ${port}`);
   });
