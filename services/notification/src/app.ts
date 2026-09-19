db4972aa2c4b4ed7be9aaa5c962ecdf325efcbf0
tejaskale99 <tejaskale1556@gmail.com>
2026-09-20 01:15:02 +0530

    feat(app): add error handling middleware to the application

--- services/notification/src/app.ts
+++ services/notification/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import errorHandler from "./common/middlewares/errorHandler.js";
 
 const app = express();
 
@@ -7,4 +8,5 @@ app.use(express.json());
 app.get("/health", (req, res) => {
   res.json({ status: "ok" });
 });
+app.use(errorHandler);
 export default app;
