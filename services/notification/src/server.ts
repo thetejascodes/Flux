3d1c7f3d05ac59cd760b6896a9155ba66d2eab17
tejaskale99 <tejaskale1556@gmail.com>
2026-09-19 19:18:53 +0530

    feat(notification): initialize notification service with Express and TypeScript
    - Added package.json scripts for development, build, and database management.
    - Updated dependencies and devDependencies for OpenTelemetry and Zod.
    - Created app.ts for Express application setup with a health check endpoint.
    - Implemented server.ts to start the Express server with environment configuration.
    - Configured TypeScript with appropriate compiler options and included/excluded files.

---
+++ services/notification/src/server.ts
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import app from "./app.js";
+const port = process.env.PORT;;
+
+const startServer = async () => {
+  app.listen(port, () => {
+    console.log(`🚀 Deliveries service running on port ${port}`);
+  });
+};
+
+startServer().catch((err) => {
+  console.error("Failed to start server", err);
+  process.exit(1);
+});
