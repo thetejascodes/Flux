ced0b2a1e5300cc74089e633d501a240ca03bf8e
tejaskale99 <tejaskale1556@gmail.com>
2026-09-26 13:58:02 +0530

    fix(db): refactor database connection to use Pool for improved management

--- services/inventory/src/common/db/index.ts
+++ services/inventory/src/common/db/index.ts
@@ -1,5 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 import config from "../config/index.js";
 import * as schema from "./schema.js";
 
-export const db = drizzle(config.database.url, { schema });
+export const pool = new Pool({ connectionString: config.database.url });
+export const db = drizzle(pool, { schema });
